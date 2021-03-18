fca81adf70724f7a67211e37364160fca8d641e6
maobike <maomaoq@gmail.com>
2021-03-18 18:15:00 -0500

    Fin seccion 11

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs =  require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async( req, res = response ) => {
     const { correo, password } = req.body;
@@ -44,7 +45,56 @@ const login = async( req, res = response ) => {
         })
     }
 
+    
 }
+
+const googleSingIn = async( req, res = response) => {
+    const { id_token } = req.body
+
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        // Si el usuario no existe
+        if ( !usuario ) {
+            // Se crea el usuario
+            const data = {
+                nombre,
+                correo,
+                password: '123',
+                img,
+                google: true
+            }
+            const usuario = new Usuario( data );
+            await usuario.save();
+        }
+        
+
+        // Si el usuario en DB esta en estado falso
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        // generar el JWT
+        const token = await generarJWT( usuario.id );
+
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es valido'
+        })
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login } = require('../controllers/auth');
+const { login, googleSingIn } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
@@ -12,6 +12,11 @@ router.post('/login', [
     check('password', 'La contraseña es obligatoria').notEmpty(),
     validarCampos
 ], login );
+
+router.post('/google', [
+    check('id_token', 'El id_token es necesario').notEmpty(),
+    validarCampos
+], googleSingIn );
 
 
 
